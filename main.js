d13be6d9deb86e3fb8ccb998a8a96f2e44d93b78
sarah-wines <sarah.lodico.wines@gmail.com>
2021-07-14 15:06:58 -0400

    set up docs and initial html, CSS and JS

---
+++ main.js
@@ -0,0 +1,50 @@
+console.log(document.getElementById('num1'));
+const num1 = document.getElementById('num1');
+const display = document.getElementById('display');
+console.log(num1);
+
+num1.addEventListener('click', function (event) {
+  console.log(event.target.innerText);
+  console.log('button clicked');
+  display.innerText += event.target.innerText;
+});
+num2.addEventListener('click', function (event) {
+  console.log(event.target.innerText);
+  console.log('button clicked');
+  display.innerText = display.innerText + event.target.innerText;
+});
+num3.addEventListener('click', function (event) {
+  console.log(event.target.innerText);
+  console.log('button clicked');
+  display.innerText += event.target.innerText;
+});
+num4.addEventListener('click', function (event) {
+  console.log(event.target.innerText);
+  console.log('button clicked');
+  display.innerText += event.target.innerText;
+});
+num5.addEventListener('click', function (event) {
+  console.log(event.target.innerText);
+  console.log('button clicked');
+  display.innerText += event.target.innerText;
+});
+num6.addEventListener('click', function (event) {
+  console.log(event.target.innerText);
+  console.log('button clicked');
+  display.innerText += event.target.innerText;
+});
+num7.addEventListener('click', function (event) {
+  console.log(event.target.innerText);
+  console.log('button clicked');
+  display.innerText += event.target.innerText;
+});
+num8.addEventListener('click', function (event) {
+  console.log(event.target.innerText);
+  console.log('button clicked');
+  display.innerText += event.target.innerText;
+});
+num9.addEventListener('click', function (event) {
+  console.log(event.target.innerText);
+  console.log('button clicked');
+  display.innerText += event.target.innerText;
+});
