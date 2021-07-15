77795c5acd3690ede0e79ac6d379154a2ba2adb9
sarah-wines <sarah.lodico.wines@gmail.com>
2021-07-15 09:25:26 -0400

    got clear to work and did styling

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const buttons = document.querySelectorAll('.button');
 const display = document.getElementById('display');
 const enter = document.getElementById('num=');
 let math = '';
+const clear = document.getElementById('numc');
+
 for (let button of buttons) {
   button.addEventListener('click', function (event) {
     display.innerText += event.target.innerText;
@@ -12,4 +14,9 @@ for (let button of buttons) {
 enter.addEventListener('click', function (event) {
   let answer = eval(math);
   display.textContent = answer;
+});
+
+clear.addEventListener('click', function (event) {
+  display.textContent = '';
+  math = '';
 });
